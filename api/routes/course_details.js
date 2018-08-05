2d111385a98a0156ed8e957bca6e1b8e6307c978
Mohtasin Meherab Sikder <30768212+Rusho-RU@users.noreply.github.com>
2018-08-06 01:31:19 +0600

    Corrected errors in module path

--- api/routes/course_details.js
+++ api/routes/course_details.js
@@ -9,22 +9,4 @@ router.get('/', (req, res, next) => {
     });
 });
 
-router.post('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling POST request for /course_details'
-    });
-});
-
-router.put('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling PUT request for /course_details'
-    });
-});
-
-router.delete('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling DELETE request for /course_details'
-    });
-});
-
 module.exports = router;
--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const course_detailsRoutes = require('.api/routes/course_details');
+const courseRoutes = require('./api/routes/course_details');
 
 app.use('/course_details', courseRoutes);
 
