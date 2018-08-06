f40cf841e97bb0d406bf7845d9a32672b803d98d
Mohtasin Meherab Sikder <30768212+Rusho-RU@users.noreply.github.com>
2018-08-07 04:26:33 +0600

    Added features from testeable features doc.

--- api/routes/course_details.js
+++ api/routes/course_details.js
@@ -4,7 +4,6 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
-
 const Course = require('../models/course_details');
 
 router.post('/:department_name/:year', (req, res, next) => {
