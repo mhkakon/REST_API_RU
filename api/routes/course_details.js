f1aaa2e2b7ce53aef7654ebb2318ddff87c81f68
Mohtasin Meherab Sikder <30768212+Rusho-RU@users.noreply.github.com>
2018-08-06 17:37:06 +0600

    Error handilng and added POST method

--- api/routes/course_details.js
+++ api/routes/course_details.js
@@ -9,4 +9,27 @@ router.get('/', (req, res, next) => {
     });
 });
 
+router.get('/:department_name', (req, res, next) => {
+    const name  = req.params.department_name;
+
+    res.status(200).json({
+        message: 'Handling GET request for /course_details',
+        id: name
+    });
+});
+
+router.post('/:department_name/:year/:semester', (req, res, next) => {
+    const course = {
+        code: req.body.code,
+        name: req.body.name,
+        credit: req.body.credit,
+        details: req.body.details
+    };
+
+    res.status(201).json({
+        message: 'Course has been added',
+        createdCourse: course
+    });
+});
+
 module.exports = router;
--- app.js
+++ app.js
@@ -2,8 +2,31 @@
 
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
 const courseRoutes = require('./api/routes/course_details');
 
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//Routes
 app.use('/course_details', courseRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found!');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
